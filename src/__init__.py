e922b13f0f0b830575d5a16ec9ca6b59335a25e7
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-06-30 16:20:46 -0600

    templating navbars

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,20 @@
+from os import environ
 from flask import Flask, render_template, redirect, url_for
+# from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.jinja_env.undefined = StrictUndefined
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"]=False
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 
 if __name__ == "__main__":
+    app.jinja_env.auto_reload = app.debug
+    # DebugToolbarExtension(app)
+    # connect_to_db(app)
     app.run(debug=True)
