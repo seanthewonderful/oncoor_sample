1502fe71d56869167d3056372702dfdfb8dcf67d
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-07-22 15:43:26 -0600

    attempt file restructure

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+from jinja2 import StrictUndefined
+# from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"]=False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///oncoorDB.db'
+# app.config["SQLALCHEMY_DATABASE_URI"] = ''
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "secretsecrets"
+csrf = CSRFProtect(app)
 
-from src import views
+from src import views, models
--- src/models.py
+++ src/models.py
@@ -1,65 +1,8 @@
-import pandas
-import csv
-import random
 from flask_sqlalchemy import SQLAlchemy
-from src.main import app
+from src import app
 
 
 db = SQLAlchemy(app)
-
-df = pandas.read_csv('src/players.csv')
-players_df = pandas.read_csv('src/players.csv', usecols=["name", "school", "sport", "img1", "img2"])
-
-
-# print(df['name'].to_list())
-# print(df.name)
-names = df.shop_item1
-# print(names[1])
-# print(df[df['name'] == 'Bella Wright'])
-
-i1 = df.shop_item1.to_list()
-i1_price = df.shop_item1_price.to_list()
-i1_img = df.shop_item1_img1.to_list()
-i1_img2 = df.shop_item1_img2.to_list()
-# i2 = df.shop_item2.to_list()
-# i2_price = df.shop_item2_price.to_list()
-# i2_img = df.shop_item2_img1.to_list()
-
-i1_list = list(zip(i1, i1_price, i1_img, i1_img2))
-# print(random.sample(i1_list, len(i1_list)))
-# print(i1_list[0][0])
-
-items = random.sample(i1_list, len(i1_list))
-
-
-with open('src/players.csv', 'r') as players:
-    player_data = list(csv.DictReader(players))
-
-
-def seed_db():
-    for each in player_data:
-        new_player = Player(
-            first_name = each['name'].split()[0],
-            last_name = each['name'].split()[1],
-            school = each['school'],
-            sport = each['sport'],
-            img1_url = each['img1'],
-            img2_url = each['img2']
-        )
-        db.session.add(new_player)
-        db.session.commit()
-        
-def seed_shop():
-    for each in player_data:
-        new_item = ShopItem(
-            name = each['shop_item1'],
-            price = each['shop_item1_price'],
-            img1_url = each['shop_item1_img1'],
-            img2_url = each['shop_item1_img2'],
-            player_id = Player.query.filter_by(last_name=each['name'].split()[1]).first().id
-        )
-        db.session.add(new_item)
-        db.session.commit()
 
 
 class Player(db.Model):
@@ -87,8 +30,31 @@ class ShopItem(db.Model):
     def __repr__(self):
         return f"Shop item {self.name}, player_id={self.player_id}"
     
-    
 
-# if __name__ == "__main__":
-#     app.jinja_env.auto_reload = app.debug
-#     app.run(debug=True)
+player_data = Player.query.all()
+shop_items = ShopItem.query.all()
+
+# def seed_db():
+#     for each in player_data:
+#         new_player = Player(
+#             first_name = each['name'].split()[0],
+#             last_name = each['name'].split()[1],
+#             school = each['school'],
+#             sport = each['sport'],
+#             img1_url = each['img1'],
+#             img2_url = each['img2']
+#         )
+#         db.session.add(new_player)
+#         db.session.commit()
+        
+# def seed_shop():
+#     for each in player_data:
+#         new_item = ShopItem(
+#             name = each['shop_item1'],
+#             price = each['shop_item1_price'],
+#             img1_url = each['shop_item1_img1'],
+#             img2_url = each['shop_item1_img2'],
+#             player_id = Player.query.filter_by(last_name=each['name'].split()[1]).first().id
+#         )
+#         db.session.add(new_item)
+#         db.session.commit()
