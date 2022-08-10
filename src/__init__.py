cef2a1b896c303e41014ab378df5db98ac093e27
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-10 17:07:04 -0600

    add heroku config vars

--- src/__init__.py
+++ src/__init__.py
@@ -4,14 +4,12 @@ from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 
-from src import DevelopmentConfig
 # app.secret_key = environ["SECRET_KEY"]
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config["SQLALCHEMY_DATABASE_URI"] = environ["SQLALCHEMY_DATABASE_URI"]
 # app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
-# env_config = environ.get("APP_SETTINGS")
-app.config.from_object(DevelopmentConfig())
+
 app.jinja_env.undefined = StrictUndefined
 
 from src import models
