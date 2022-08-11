8a3b15276f75111ebd78a53b0adec23e82b92fca
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-11 11:41:54 -0600

    new day

--- src/__init__.py
+++ src/__init__.py
@@ -5,15 +5,15 @@ from jinja2 import StrictUndefined
 app = Flask(__name__)
 
 # app.secret_key = environ["SECRET_KEY"]
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = environ["POSTGRES_URI"]
 # app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 app.jinja_env.undefined = StrictUndefined
 
 from src import models
-models.connect_to_db(app)
 from src import views, forms
+models.connect_to_db(app)
 
 
 if __name__ == "__main__":
